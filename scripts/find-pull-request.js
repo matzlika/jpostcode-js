4c311254009043b371eeb5eb67cbbf0af37610a5
Taro KOBAYASHI <9re.3000@gmail.com>
2025-03-03 01:43:05 +0900

    changed to proper commit message

--- scripts/find-pull-request.js
+++ scripts/find-pull-request.js
@@ -1,4 +1,4 @@
 fetch('https://api.github.com/repos/kufu/jpostcode-data/pulls')
   .then(r => r.json())
-  .then(pullRequests => pullRequests.find(({title}) => title === 'bundle exec rake jpostcode:data:update_allx'))
+  .then(pullRequests => pullRequests.find(({title}) => title === 'bundle exec rake jpostcode:data:update_all'))
   .then(pr => console.log(pr?.number));
